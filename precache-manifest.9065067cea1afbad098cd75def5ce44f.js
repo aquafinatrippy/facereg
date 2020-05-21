396978ae9ca543a4a658c59ee3c4e41fe38160aa
Tanel Tekko <taneldeviis@hotmail.com>
2020-05-21 22:33:39 +0300

    Updates

---
+++ precache-manifest.9065067cea1afbad098cd75def5ce44f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0943dd227e6255da5d797bbe1d93bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33984d4cf9d4c81ef68",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a164c874dec7481fc765",
+    "url": "/static/css/main.2535eebc.chunk.css"
+  },
+  {
+    "revision": "b33984d4cf9d4c81ef68",
+    "url": "/static/js/2.ef9827f4.chunk.js"
+  },
+  {
+    "revision": "a164c874dec7481fc765",
+    "url": "/static/js/main.f9ee6722.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
